89e8198ed708540e3ea97594fc3d52f7fb6ca719
Terceiro <alexandre.senaidstatui@gmail.com>
2026-02-05 13:30:39 -0300

    realizado

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,21 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Testcontroller;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/teste',[Testcontroller::class,'envia_teste']);
+
+
+Route::get('/soma',[Testcontroller::class,'soma']);
+
+
+Route::post('/salva_carro',[Testcontroller::class,'salva_carro']);
+
+
+Route::get('/exibe_carro/{id}',[Testcontroller::class,'exibe_carro']);
+
+
+Route::get('/todos_carros',[Testcontroller::class,'todos_carros']);
